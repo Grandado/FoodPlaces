700149af074f90f186765e6a65f11f12f0f57f43
Guilherme Nascimento <57995773+Grandado@users.noreply.github.com>
2024-01-19 14:49:37 -0300

    refatoração para es5

--- src/const.js
+++ src/const.js
@@ -1,7 +1,13 @@
 import { config } from 'dotenv';
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'url';
+
 
 // Load config from .env
 config();
 
 export const API_PREFIX = process.env.API_PREFIX || '/api';
 export const TOKEN = process.env.TOKEN;
+export const CLIENT_ID = process.env.CLIENT_ID;
+export const GUILD_ID = process.env.GUILD_ID;
+export const __dirname = dirname(fileURLToPath(import.meta.url));
--- src/index.js
+++ src/index.js
@@ -1,26 +1,15 @@
-import { join, dirname } from 'path';
-import { fileURLToPath } from 'url';
 import { readdirSync } from 'node:fs';
-import express from 'express';
-import routes from './routes/index.js';
-import { API_PREFIX, TOKEN } from './const.js';
+import { join } from 'node:path';
+import { TOKEN, __dirname } from './const.js';
 import { Client, Collection, Events, GatewayIntentBits } from 'discord.js';
 
-const __dirname = dirname(fileURLToPath(new URL('.', import.meta.url)));
-
-const app = express();
-
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds /* 
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent, */,
-  ],
+  intents: [GatewayIntentBits.Guilds],
 });
 
 client.commands = new Collection();
 
-const foldersPath = join(__dirname, 'commands');
+const foldersPath = join(__dirname, '..', 'commands');
 const commandFolders = readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
@@ -28,25 +17,19 @@ for (const folder of commandFolders) {
   const commandFiles = readdirSync(commandsPath).filter((file) =>
     file.endsWith('.js')
   );
-
   for (const file of commandFiles) {
-    const filePath = join('file://', commandsPath, file);
+    const filePath = join('file:', commandsPath, file);
     const { default: command } = await import(filePath);
-
-    console.log(command);
     // Set a new item in the Collection with the key as the command name and the value as the exported module
-    /* if ('data' in command && 'execute' in command) {
+    if ('data' in command && 'execute' in command) {
       client.commands.set(command.data.name, command);
     } else {
       console.log(
         `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
       );
-    } */
+    }
   }
 }
-
-app.use(express.json({ limit: '5mb' }));
-app.use(express.urlencoded({ extended: false }));
 
 client.once(Events.ClientReady, (readyClient) => {
   console.log(`Ready! Logged in as ${readyClient.user.tag}`);
@@ -85,7 +68,3 @@ client.on(Events.InteractionCreate, async (interaction) => {
 });
 
 client.login(TOKEN);
-
-app.use(API_PREFIX, routes);
-
-export default app;
